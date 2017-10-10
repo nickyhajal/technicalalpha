cd70d2d4e8319fbca46891612f0d530378c77c07
Nicky Hajal <nhajal@gmail.com>
2017-10-10 10:49:43 -0500

    pkg

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 const express = require('express');
 const server = express();
+
 app.prepare().then(() => {
   server.get('/api/assets', assets);
   server.get('/api/redirectUrl', getRedirectUrl);
