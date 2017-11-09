942de8e9d5502baa808861d035a30dc68487748c
Nicky Hajal <nhajal@gmail.com>
2017-11-09 10:44:35 -0600

    use patreonwall json

--- utils/getPatrons.js
+++ utils/getPatrons.js
@@ -4,6 +4,7 @@ const getPatrons = () =>
   new Promise((resolve, reject) => {
     const nums = { god: 1, fb: 2, oprah: 3, exec: 15 };
     fs.readFile('/var/www/content/patreonwall.json', (err, data) => {
+      console.log(data);
       const patrons = JSON.parse(data);
       Object.keys(patrons).forEach(k => {
         const avail = nums[k] - patrons[k].length;
