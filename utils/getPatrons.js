90cebd93de1720821ab62bad73407414035983a6
Nicky Hajal <nhajal@gmail.com>
2017-11-09 10:59:14 -0600

    use patreonwall json

--- utils/getPatrons.js
+++ utils/getPatrons.js
@@ -1,21 +1,32 @@
 const fs = require('fs');
 
+let patrons = {};
+let lastFetched = 0;
+
 const getPatrons = () =>
   new Promise((resolve, reject) => {
+    const now = +new Date();
     const nums = { god: 1, fb: 2, oprah: 3, exec: 15 };
-    fs.readFile(
-      '/var/www/technicalalpha/content/patreonwall.json',
-      (err, data) => {
-        const patrons = JSON.parse(data);
-        Object.keys(patrons).forEach(k => {
-          const avail = nums[k] - patrons[k].length;
-          for (let i = 0; i < avail; i++) {
-            patrons[k].push('Available');
-          }
-        });
-        resolve(patrons);
-      },
-    );
+    const finish = () => {
+      Object.keys(patrons).forEach(k => {
+        const avail = nums[k] - patrons[k].length;
+        for (let i = 0; i < avail; i++) {
+          patrons[k].push('Available');
+        }
+      });
+      resolve(patrons);
+    };
+    if (now - lastFetched > 60000) {
+      fs.readFile(
+        '/var/www/technicalalpha/content/patreonwall.json',
+        (err, data) => {
+          patrons = JSON.parse(data);
+          finish();
+        },
+      );
+    } else {
+      finish();
+    }
   });
 
 module.exports = getPatrons;
