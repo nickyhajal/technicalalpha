7da11b4c15362de24821c4bb051efffd17ab9e21
Nicky Hajal <nhajal@gmail.com>
2017-11-09 10:35:53 -0600

    use patreonwall json

--- utils/getPatrons.js
+++ utils/getPatrons.js
@@ -1,30 +1,35 @@
+const fs = require('fs');
+
 const getPatrons = () =>
   new Promise((resolve, reject) => {
     const nums = { god: 1, fb: 2, oprah: 3, exec: 15 };
-    const patrons = {
-      god: [],
-      fb: ['Jetrix'],
-      oprah: ['BSTULLKIT'],
-      exec: [
-        'JoeMcDong',
-        'Sir Homes',
-        'Postal Panda',
-        'Jamaican Jazz',
-        'Adam B',
-        'ToadOfSteel',
-        'Naeli',
-        'Derek P',
-        'Frysiee',
-        'IceWulfie',
-      ],
-    };
-    Object.keys(patrons).forEach(k => {
-      const avail = nums[k] - patrons[k].length;
-      for (let i = 0; i < avail; i++) {
-        patrons[k].push('Available');
-      }
+    fs.readFile('/var/www/content/patreonwall.json', (err, data) => {
+      const patrons = JSON.parse(data);
+      Object.keys(patrons).forEach(k => {
+        const avail = nums[k] - patrons[k].length;
+        for (let i = 0; i < avail; i++) {
+          patrons[k].push('Available');
+        }
+      });
+      resolve(patrons);
     });
-    resolve(patrons);
+    // const patrons = {
+    //   god: [],
+    //   fb: ['Jetrix'],
+    //   oprah: ['BSTULLKIT'],
+    //   exec: [
+    //     'JoeMcDong',
+    //     'Sir Homes',
+    //     'Postal Panda',
+    //     'Jamaican Jazz',
+    //     'Adam B',
+    //     'ToadOfSteel',
+    //     'Naeli',
+    //     'Derek P',
+    //     'Frysiee',
+    //     'IceWulfie',
+    //   ],
+    // };
   });
 
 module.exports = getPatrons;
